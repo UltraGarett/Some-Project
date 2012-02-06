323fc266429d0d04ef76d153af4b1681d403221d
Cool Down <g_computation@hotmail.com>
2012-02-05 22:58:48 -0700

    First Commit

---
+++ src/bla/foo/bar/cool/fool/mule/SomeProjectActivity.java
@@ -0,0 +1,13 @@
+package bla.foo.bar.cool.fool.mule;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SomeProjectActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
